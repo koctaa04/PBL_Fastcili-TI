6331f875c76ea4ccffeb7e8bc5e365968123227b
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-05-09 15:01:31 +0700

    fix: memperbaiki bug pada tampilan dashboard tidak rapi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,8 +44,7 @@ The above copyright notice and this permission notice shall be included in all c
     <!-- CSS Files -->
     <link href="{{ asset('paper') }}/css/bootstrap.min.css" rel="stylesheet" />
     <link href="{{ asset('paper') }}/css/paper-dashboard.css?v=2.0.0" rel="stylesheet" />
-    {{-- style landing page --}}
-    <link rel="stylesheet" href="{{ asset('landing/style.css') }}"> 
+    
     {{-- <!-- CSS Just for demo purpose, don't include it in your project -->
     <link href="{{ asset('paper') }}/demo/demo.css" rel="stylesheet" /> --}}
 </head>
--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -1,3 +1,4 @@
 @include('layouts.navbars.navs.guest')
-
+{{-- style landing page --}}
+<link rel="stylesheet" href="{{ asset('landing/style.css') }}">
 @yield('content')
