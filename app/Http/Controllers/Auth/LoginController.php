74beb74df8e1b435cf67e78ba54dcfbad4110d23
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-05-09 12:58:46 +0700

    feat: menambahkan method untuk fitur 'Remember Me' pada halaman Login dapat berfungsi

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,20 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if ($request->remember) {
+            // Set cookies for 30 days (43200 minutes)
+            Cookie::queue('remember_email', $request->email, 43200);
+            Cookie::queue('remember_password', $request->password, 43200);
+        } else {
+            // Forget the cookies if "Remember me" is not checked
+            Cookie::queue(Cookie::forget('remember_email'));
+            Cookie::queue(Cookie::forget('remember_password'));
+        }
+
+        return redirect()->intended($this->redirectPath());
     }
 }
