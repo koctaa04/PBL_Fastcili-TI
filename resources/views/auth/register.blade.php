321c97d6028f27477ac9adff3f5077025ed32be6
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-05-09 12:55:39 +0700

    style: redesign untuk halaman login dan register - menambahkan bg-video - menambahkan overlay untuk bg-video - menambahkan syarat & ketentuan pada halaman register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,74 +4,78 @@
 ])
 
 @section('content')
-    <div class="content">
-        <div class="container">
+<div class="wrapper">
+    <!-- Background Video-->
+    <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+        <source src="{{ asset('mp4/bg-video.mp4') }}" type="video/mp4" />
+    </video>
+    
+    
+    <!-- Gradient Overlay -->
+        <div class="gradient-overlay"></div>    
+
+        <div class="container-login">
             <div class="row">
-                <div class="col-lg-5 col-md-5 ml-auto">
-                    <div class="info-area info-horizontal mt-5">
-                        <div class="icon icon-primary">
-                            <i class="nc-icon nc-tv-2"></i>
-                        </div>
+                <div class="col-lg-7 col-md-5 ml-auto">
+                    <h2 class="description info-title mb-2">Syarat dan Ketentuan</h2>
+                    <div class="terms info-horizontal">
                         <div class="description">
-                            <h5 class="info-title">{{ __('Marketing') }}</h5>
+                            <h5 class="info-title mb-0">1. Akses dan penggunaan</h5>
                             <p class="description">
-                                {{ __('We\'ve created the marketing campaign of the website. It was a very interesting collaboration.') }}
+                                <ul>
+                                    <li>Sistem ini hanya dapat digunakan oleh civitas akademika Politeknik Negeri Malang, termasuk mahasiswa, dosen, dan staf.</li>
+                                    <li>Pengguna wajib menjaga kerahasiaan kredensial.</li>
+                                    <li>Penyalahgunaan sistem untuk laporan palsu, spam, atau informasi tidak relevan akan dikenakan sanksi sesuai ketentuan kampus.</li>
+                                </ul>
                             </p>
                         </div>
                     </div>
-                    <div class="info-area info-horizontal">
-                        <div class="icon icon-primary">
-                            <i class="nc-icon nc-html5"></i>
-                        </div>
+                    <div class="terms info-horizontal">
                         <div class="description">
-                            <h5 class="info-title">{{ __('Fully Coded in HTML5') }}</h5>
+                            <h5 class="info-title mb-0">2. Tanggung Jawab Pengguna</h5>
                             <p class="description">
-                                {{ __('We\'ve developed the website with HTML5 and CSS3. The client has access to the code using GitHub.') }}
+                                <ul>
+                                    <li>Pengguna bertanggung jawab atas kebenaran dan keakuratan informasi yang dilaporkan.</li>
+                                    <li>Setiap laporan yang dikirim akan melalui proses validasi dan dapat ditindaklanjuti oleh Tim Sarana dan Prasarana (Sarpras) kampus.</li>
+                                    <li>Pengguna dilarang mengunggah konten yang bersifat ofensif, diskriminatif, atau melanggar hukum.</li>
+                                </ul>
                             </p>
                         </div>
                     </div>
-                    <div class="info-area info-horizontal">
-                        <div class="icon icon-info">
-                            <i class="nc-icon nc-atom"></i>
-                        </div>
+                    <div class="terms info-horizontal">
                         <div class="description">
-                            <h5 class="info-title">{{ __('Built Audience') }}</h5>
+                            <h5 class="info-title mb-0">3. Hak dan Batasan</h5>
                             <p class="description">
-                                {{ __('There is also a Fully Customizable CMS Admin Dashboard for this product.') }}
+                                <ul>
+                                    <li>Tim pengembang berhak memperbarui sistem, memperbaiki bug, atau menambahkan fitur baru tanpa pemberitahuan terlebih dahulu.</li>
+                                    <li>Pengembang tidak bertanggung jawab atas kerugian akibat penggunaan sistem yang tidak sesuai dengan ketentuan ini.</li>
+                                </ul>
                             </p>
                         </div>
+                    </div>
+                    <div class="terms info-horizontal">
+                        <p class="mt-2 description text-end"><small>Terakhir diperbarui: {{ now()->format('d F Y') }}</small></p>
                     </div>
                 </div>
+                
                 <div class="col-lg-4 col-md-6 mr-auto">
                     <div class="card card-signup text-center">
                         <div class="card-header ">
                             <h4 class="card-title">{{ __('Register') }}</h4>
-                            <div class="social">
-                                <button class="btn btn-icon btn-round btn-twitter">
-                                    <i class="fa fa-twitter"></i>
-                                </button>
-                                <button class="btn btn-icon btn-round btn-dribbble">
-                                    <i class="fa fa-dribbble"></i>
-                                </button>
-                                <button class="btn btn-icon btn-round btn-facebook">
-                                    <i class="fa fa-facebook-f"></i>
-                                </button>
-                                <p class="card-description">{{ __('or be classical') }}</p>
-                            </div>
                         </div>
                         <div class="card-body ">
                             <form class="form" method="POST" action="{{ route('register') }}">
                                 @csrf
-                                <div class="input-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                                <div class="input-group{{ $errors->has('nama') ? ' has-danger' : '' }}">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text">
                                             <i class="nc-icon nc-single-02"></i>
                                         </span>
                                     </div>
-                                    <input name="name" type="text" class="form-control" placeholder="Name" value="{{ old('name') }}" required autofocus>
-                                    @if ($errors->has('name'))
+                                    <input name="nama" type="text" class="form-control" placeholder="Nama Lengkap" value="{{ old('nama') }}" required autofocus>
+                                    @if ($errors->has('nama'))
                                         <span class="invalid-feedback" style="display: block;" role="alert">
-                                            <strong>{{ $errors->first('name') }}</strong>
+                                            <strong>{{ $errors->first('nama') }}</strong>
                                         </span>
                                     @endif
                                 </div>
@@ -107,7 +111,7 @@
                                             <i class="nc-icon nc-key-25"></i>
                                         </span>
                                     </div>
-                                    <input name="password_confirmation" type="password" class="form-control" placeholder="Password confirmation" required>
+                                    <input name="password_confirmation" type="password" class="form-control" placeholder="Konfirmasi Password" required>
                                     @if ($errors->has('password_confirmation'))
                                         <span class="invalid-feedback" style="display: block;" role="alert">
                                             <strong>{{ $errors->first('password_confirmation') }}</strong>
@@ -118,8 +122,8 @@
                                     <label class="form-check-label">
                                         <input class="form-check-input" name="agree_terms_and_conditions" type="checkbox">
                                         <span class="form-check-sign"></span>
-                                            {{ __('I agree to the') }}
-                                        <a href="#something">{{ __('terms and conditions') }}</a>.
+                                            {{ __('Saya setuju dengan') }}
+                                        <a href="#lihatkiriformmas">{{ __('syarat dan ketentuan') }}</a>.
                                     </label>
                                     @if ($errors->has('agree_terms_and_conditions'))
                                         <span class="invalid-feedback" style="display: block;" role="alert">
@@ -134,9 +138,10 @@
                         </div>
                     </div>
                 </div>
-             </div>
+                </div>
         </div>
-     </div> 
+    </div>
+    
 @endsection
 
 @push('scripts')
