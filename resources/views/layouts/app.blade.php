556224ec97e378be0af452cb46a5b6dd8a4bf2e1
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-06-09 09:35:24 +0700

    fix: memperbaiki nav menu tidak dapat muncul untuk layar kecil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@ The above copyright notice and this permission notice shall be included in all c
     <!--  Notifications Plugin    -->
     <script src="{{ asset('paper') }}/js/plugins/bootstrap-notify.js"></script>
     <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-    {{-- <script src="{{ asset('paper') }}/js/paper-dashboard.min.js?v=2.0.0" type="text/javascript"></script> --}}
+    <script src="{{ asset('paper') }}/js/paper-dashboard.min.js?v=2.0.0" type="text/javascript"></script>
     <!-- Paper Dashboard DEMO methods, don't include it in your project! -->
     <script src="{{ asset('paper') }}/demo/demo.js"></script>
     <!-- Sweetalert -->
