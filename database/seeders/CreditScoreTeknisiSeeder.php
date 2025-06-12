1c7f1c682c038871a0781489af45b743c9ab7605
koctaa04 <yeftaocta04@gmail.com>
2025-06-12 12:07:00 +0700

    feat: menampilkan sweet alert pada ganti teknisi dan mengubah credit score teknisi

---
+++ database/seeders/CreditScoreTeknisiSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CreditScoreTeknisi;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreditScoreTeknisiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        CreditScoreTeknisi::insert([
+            ['id_user' => 5],
+            ['id_user' => 6],
+            ['id_user' => 7],
+            ['id_user' => 8],
+            ['id_user' => 9],
+        ]);
+    }
+}
